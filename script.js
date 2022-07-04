e8ee1f3cd4d013e2b092f437b878d154161231d0
Kawayan <kawayan.deguzman@gmail.com>
2022-07-04 12:49:01 -0700

    querySelector for data-equals

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelectorAll('[data-equals]')
