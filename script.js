e99cf051c6d8623ba9e4ff912ca6c29da6f963dc
Kawayan <kawayan.deguzman@gmail.com>
2022-07-06 16:16:35 -0700

    create query selector for delete button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
