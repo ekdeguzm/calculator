12826b9e53b7aab18baf90d25eea1657ee0026fa
Kawayan <kawayan.deguzman@gmail.com>
2022-07-11 15:57:19 -0700

    change isNan to isNaN in compute function

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ class Calculator {
     let computation
     const prev = parseFloat(this.previousOperand)
     const current = parseFloat(this.currentOperand)
-    if (isNan(prev) || isNaN(current)) return
+    if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
       case '+':
         computation = prev + current
@@ -64,8 +64,8 @@ class Calculator {
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-delete-all]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
